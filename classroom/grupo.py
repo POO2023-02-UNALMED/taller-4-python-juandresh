af312e4ce4a1e4f1796aa9206186fedb45597f65
juandresh <141792824+juandresh@users.noreply.github.com>
2023-09-02 21:36:12 -0500

    Update grupo.py

--- classroom/grupo.py
+++ classroom/grupo.py
@@ -3,7 +3,7 @@ from classroom.asignatura import Asignatura
 class Grupo:
     grado = None
 
-    def __init__(self, grupo="grupo predeterminado", asignaturas=None, estudiantes=None):
+    def __init__(self, grupo="grupo predeterminado", asignaturas=[], estudiantes=[]):
         self._grupo = grupo
         self._asignaturas = asignaturas if asignaturas is not None else []
         self.listadoAlumnos = estudiantes if estudiantes is not None else []
